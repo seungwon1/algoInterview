80461a30a6d85280e037c74a3722247fc5601caf
seungwon1 <pennymagic156@gmail.com>
2021-11-07 22:49:33 +0900

    Add files via upload

---
+++ bitManipulation.py
@@ -0,0 +1,22 @@
+import collections
+
+"""
+1. Maximum Subarray XOR
+2. Magic Number
+3. Sum of bit differences among all pairs
+4. Swap All Odds And Even Bits
+5. Find the element that appears once
+6. Binary representation of a given number
+7. Count total set bits in all numbers from 1 to n
+8. Rotate bits of a number
+9. Count number of bits to be flipped to convert A to B
+10. Find Next Sparse Number
+"""
+
+
+# 1. Maximum Subarray XOR
+# naive approach: iterate over all pairs of (i,j) s.t. 0<= i <= j <= n
+def maxSubarrayXOR(arr, n):
+
+
+    return
