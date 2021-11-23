9bb307a65b8101617f1c3bd470224a46d41a25cd
seungwon1 <pennymagic156@gmail.com>
2021-11-23 19:34:00 +0900

    Add files via upload

--- stringArray.py
+++ stringArray.py
@@ -100,8 +100,54 @@ def zigZag(arr, n):
 
 
 # 5. Generate all possible sorted arrays from alternate elements of two given sorted arrays
+# backtracking: O((M+N)2^(M+N)) time and O(2^(M+N)) space
+# better approach: O(N**2) time and O(M+N) space w/ recursion ??
 def generate(A, B, m, n):
-    return
+    def mergeList(A, B):
+        i, j, m, n = 0, 0, len(A), len(B)
+        AB = []
+        while i < m and j < n:
+            if A[i] <= B[j]:
+                AB.append((A[i], 0))
+                i += 1
+            else:
+                AB.append((B[j], 1))
+                j += 1
+        while i < m:
+            AB.append((A[i], 0))
+            i += 1
+        while j < n:
+            AB.append((B[j], 1))
+            j += 1
+        return AB
+
+    AB = mergeList(A, B)
+    sortedArr = set()
+
+    def backtrack(idx, tmp):
+        v = tuple(tmp)
+        if v in sortedArr:
+            return
+
+        if len(tmp) % 2 == 0 and tmp:
+            print(tmp)
+            sortedArr.add(v)
+
+        for i in range(idx, m + n):
+            if len(tmp) % 2 == 0:
+                if AB[i][1] == 0 and (not tmp or AB[i] > tmp[-1]):
+                    tmp.append(AB[i][0])
+                    backtrack(i + 1, tmp)
+                    tmp.pop()
+            else:
+                if AB[i][1] == 1 and AB[i] > tmp[-1]:
+                    tmp.append(AB[i][0])
+                    backtrack(i + 1, tmp)
+                    tmp.pop()
+        return
+
+    backtrack(0, [])
+    return sortedArr
 
 
 # 6. Pythagorean Triplet in an array
@@ -186,9 +232,47 @@ def smallestSubWithSum(arr, n, x):
     return minLength
 
 
-# 9-2. follow-up: with negative values (arr element and sum value)
-def subArraySum(arr, n, summ):
-    return
+# 9-2. follow-up
+# 1) # of subarray cums up to the given value - O(N) time and O(N) space w/ hashmap
+# 2) find the subarray that sums up to the given value - O(N) time and O(1) space
+# BF: O(n**2) time and O(n) space using prefix sum and iterate over all (i,j) pairs
+# better solution: linear time and constant space
+
+# 9-2-1
+def subArraySumHashmap(arr, n, s):
+    prefixSum = collections.defaultdict(set)
+    curr = 0
+    ans = 0
+    for i, v in enumerate(arr):
+        curr += v
+        if curr == s:
+            ans += 1
+        if curr - s in prefixSum:
+            ans += len(prefixSum[curr - s])
+        prefixSum[curr].add(i)
+    return ans
+
+
+# 9-2-2 ***
+# convert negative values into positive values and do linear scan w/ sliding window technique
+def subArraySum(arr, n, s):
+    minValue = abs(min(arr))
+    arr = [e + minValue for e in arr]
+    p1 = 0
+    curr = arr[0]
+    for i in range(1, n+1):
+        while curr > (i - p1) * minValue + s and p1 < i:
+            curr -= arr[p1]
+            p1 += 1
+
+        if curr - (i - p1) * minValue == s:
+            return p1, i
+
+        if i < n:
+            curr += arr[i]
+
+    return -1
+
 
 
 # 10. Stock Buy Sell to Maximize Profit
