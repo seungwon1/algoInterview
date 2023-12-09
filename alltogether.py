7a04298be9d254e946ccf3dc52b20e40148103bf
seungwon1 <pennymagic156@gmail.com>
2023-12-09 18:39:24 +0900

    Update alltogether.py

--- alltogether.py
+++ alltogether.py
@@ -268,6 +268,7 @@ class LazySegTree:
         l += self.size
         r += self.size
         # lazy prop from the root
+        # not push if the whole range is convered at a particular level
         for i in range(self.log, 0, -1):
             if ((l >> i) << i) != l:
                 self._push(l >> i)
@@ -323,6 +324,7 @@ class LazySegTree:
             r >>= 1
         l, r = l2, r2
         # update from the leaf
+        # not push if the whole range is convered at a particular level
         for i in range(1, self.log + 1):
             if ((l >> i) << i) != l:
                 self._update(l >> i)
