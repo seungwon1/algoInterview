60fcf2876a31b91fb6fdd2f6cb8f133dcaad335e
seungwon1 <pennymagic156@gmail.com>
2024-01-22 20:00:08 +0900

    Update tem.cpp

--- c/tem.cpp
+++ c/tem.cpp
@@ -12,3 +12,13 @@
 #include <functional>
 #include <set>
 using namespace std;
+
+vector<int> readList(int n) {
+    vector<int> result(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> result[i];
+    }
+    return result;
+}
+
+
