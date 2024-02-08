e4a705261073a8c732e3bcdebdf52436b4df0ad0
seungwon1 <pennymagic156@gmail.com>
2024-02-08 19:02:31 +0900

    Create scc_Tarjan.py

---
+++ graph/scc_Tarjan.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(5 * 10 ** 4)
+
+# Strongly Connected Components: Tarjan's Algorithm
+n = 2* (10**5)
+graph = [[] for _ in range(n)]
+scc = [[] for _ in range(n)]
+clk = [0]
+def findSCC(node, tin, low, st, ist):
+    tin[node] = low[node] = clk[0]
+    clk[0] += 1
+    ist[node] = 1
+    for to in graph[node]:
+        if tin[to] == -1:
+            findSCC(to, tin, low, st, ist)
+            low[node] = min(low[node], low[to])
+        elif ist[to]:
+            low[node] = min(low[node], tin[node])
+
+    if tin[node] == low[node]:
+        root = node
+        while st and st[-1] != root:
+            scc[root].append(st.pop())
+            ist[scc[root][-1]] = 1
+        scc[root].append(st.pop())
+        ist[scc[root][-1]] = 1
+
+def SCC():
+    tin = [-1] * n
+    low = [-1] * n
+    st = []
+    ist = [0] * n
+    for i in range(n):
+        if tin[i] == -1:
+            findSCC(i, tin, low, st, ist)
+
+
+
