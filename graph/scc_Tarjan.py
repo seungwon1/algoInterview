e0326a041b5bff8a026cdad4c79df443e41ddc48
seungwon1 <pennymagic156@gmail.com>
2024-02-08 20:19:37 +0900

    Update scc_Tarjan.py

--- graph/scc_Tarjan.py
+++ graph/scc_Tarjan.py
@@ -1,38 +1,74 @@
 import sys
-sys.setrecursionlimit(5 * 10 ** 4)
+input = sys.stdin.readline
 
-# Strongly Connected Components: Tarjan's Algorithm
-n = 2* (10**5)
-graph = [[] for _ in range(n)]
-scc = [[] for _ in range(n)]
+n = 10**5
+tin = [n+1] * n
+low = [n+1] * n
+vis = [0] * n
 clk = [0]
-def findSCC(node, tin, low, st, ist):
-    tin[node] = low[node] = clk[0]
+scc = [[] for _ in range(n)]
+stk = []
+inStk = [0] * n
+graph = [[] for _ in range(n)]
+par = [-1] * n
+
+def dfs(n):
+    for i in range(n):
+        if not vis[i]:
+            dfsVisit_it(i)
+
+def dfsVisit_it(node):
+    st = [(node, 0)]
+    while st:
+        node, exp = st.pop()
+        if exp:
+            for to in graph[node]:
+                if par[to] == node:
+                    low[node] = min(low[node], low[to])
+            if tin[node] == low[node]:
+                while stk and stk[-1] != node:
+                    scc[node].append(stk.pop())
+                    inStk[scc[node][-1]] = 0
+                scc[node].append(stk.pop())
+                inStk[scc[node][-1]] = 0
+            continue
+
+        if vis[node]:
+            continue
+
+        st.append((node, 1))
+        vis[node] = 1
+        clk[0] += 1
+        tin[node] = low[node] = clk[0]
+        stk.append(node)
+        inStk[node] = 1
+        for to in graph[node]:
+            if not vis[to]:
+                st.append((to, 0))
+                par[to] = node
+            elif inStk[to]:
+                low[node] = min(low[node], tin[to])
+
+
+def dfsVisit_rc(node):
     clk[0] += 1
-    ist[node] = 1
+    tin[node], low[node] = clk[0], clk[0]
+    vis[node] = 1
+    stk.append(node)
+    inStk[node] = 1
     for to in graph[node]:
-        if tin[to] == -1:
-            findSCC(to, tin, low, st, ist)
+        if not vis[to]:
+            dfsVisit_rc(to)
             low[node] = min(low[node], low[to])
-        elif ist[to]:
-            low[node] = min(low[node], tin[node])
+        elif inStk[to]:
+            low[node] = min(low[node], tin[to])
 
     if tin[node] == low[node]:
-        root = node
-        while st and st[-1] != root:
-            scc[root].append(st.pop())
-            ist[scc[root][-1]] = 1
-        scc[root].append(st.pop())
-        ist[scc[root][-1]] = 1
-
-def SCC():
-    tin = [-1] * n
-    low = [-1] * n
-    st = []
-    ist = [0] * n
-    for i in range(n):
-        if tin[i] == -1:
-            findSCC(i, tin, low, st, ist)
+        while stk and stk[-1] != node:
+            scc[node].append(stk.pop())
+            inStk[scc[node][-1]] = 0
+        scc[node].append(stk.pop())
+        inStk[scc[node][-1]] = 0
 
 
 
